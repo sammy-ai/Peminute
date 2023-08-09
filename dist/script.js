dab1d2f939640e96c02f7ff1db51a3c1a528e59f
Sammy-ai <67149877+sammy-ai@users.noreply.github.com>
2023-08-09 00:02:02 -0700

    Add files via upload

--- dist/script.js
+++ dist/script.js
@@ -13,3 +13,83 @@ form.addEventListener('submit', e => {
         });
 });
 
+function validation()
+{
+    var form = document.getElementById("dataform");
+    var email = document.getElementById("Email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern))
+    {
+        text.innerHTML = "Your Email is Valid.";
+        text.style.color = "#00fe00";
+    }
+    else
+    {
+        text.innerHTML = "Please provide a valid email address.";
+        text.style.color = "#ff0000";
+    }
+
+    if (email == "")
+    {
+        form.classList.remove("text-green-500");
+        form.classList.remove("text-red-500");
+        text.innerHTML = "";
+        text.style.color = "#00ff00";
+    }
+}
+
+//SHARE BUTTONS
+// WHATSAPP
+function shareOnWhatsApp() {
+  // Replace 'YOUR_MESSAGE' with the message you want to accompany the link.
+  // For example, "Check out this cool website!"
+  const message = 'YOUR_MESSAGE';
+  const url = encodeURIComponent(window.location.href);
+  const whatsappUrl = `https://api.whatsapp.com/send?text=${message} ${url}`;
+
+  // Open WhatsApp in a new tab with the share link
+  window.open(whatsappUrl, '_blank');
+}
+
+
+// TWITTER
+function shareOnTwitter() {
+  // Replace 'YOUR_MESSAGE' with the message you want to accompany the link.
+  // For example, "Check out this cool website!"
+  const message = 'YOUR_MESSAGE';
+  const url = encodeURIComponent(window.location.href);
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${message} ${url}`;
+
+  // Open Twitter in a new tab with the tweet pre-filled
+  window.open(twitterUrl, '_blank');
+}
+
+// ADD EVENT LISTENERS
+// Add event listener to the WhatsApp Share Button
+document.getElementById("whatsapp").addEventListener("click", shareOnWhatsApp);
+// Add event listener to the Twitter Share Button
+document.getElementById("twitter").addEventListener("click", shareOnTwitter);
+
+
+
+
+/*
+//FACEBOOK
+// Function to share the current page's link on Facebook
+function shareOnFacebook() {
+  // Replace 'YOUR_MESSAGE' with the message you want to accompany the link.
+  // For example, "Check out this cool website!"
+  const message = 'YOUR_MESSAGE';
+  const url = encodeURIComponent(window.location.href);
+  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?quote=${message}&u=${url}`;
+
+  // Open Facebook in a new tab with the post pre-filled
+  window.open(facebookUrl, '_blank');
+}
+
+// Add event listener to the Facebook Share Button
+document.getElementById("facebookShareBtn").addEventListener("click", shareOnFacebook);
+*/
+
